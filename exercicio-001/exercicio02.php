aa7b46235de8bf0eeeaafd6f40f139d13c9871d3
José Wennas <josewennas@hotmail.com>
2024-03-20 13:17:40 -0300

    adicionando exercicio 03

--- exercicio-001/exercicio02.php
+++ exercicio-001/exercicio02.php
@@ -1,5 +1,65 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
 
-$a = (5==5);
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-echo $a;
+<body>
+    <?php
+    $nome = "Wennas";
+    // echo "$nome";
+    if ($nome == "Tania") {
+        echo "Vc acertou o nome";
+    } else {
+        echo "Você errou o nome";
+    }
+
+    $num1 = 5;
+    $num2 = 8;
+    $num3 = 2;
+    $num4 = 10;
+
+    $soma = $num1 + $num3;
+    print "<br>A soma é $soma";
+
+    $subtracao = $num4 - $num2;
+    print "<br>A subtracão é $subtracao";
+
+    $multiplicacao = $num1 * $num3;
+    print "<br>A multiplicação é $multiplicacao";
+
+    $divisao = $num4 / $num3;
+    print "<br>A divisão é $divisao";
+
+    //Exercício 01############################################################
+    echo "===============<br>";
+    $a = 1000;
+    $b = $a / 2;
+
+    echo "$b";
+
+    echo "<br>";
+
+    $a = 100;
+    $b = $a * 3;
+    echo $b;
+
+    echo "<br>";
+    $a = 20;
+    echo ++$a;
+    echo "<br>";
+
+    $a = 100;
+    $b = 250;
+    $c = $a + $b + 1;
+    
+    echo $c;
+
+
+    ?>
+</body>
+
+</html>
